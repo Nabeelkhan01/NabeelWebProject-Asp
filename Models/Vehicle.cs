1b336ca21c85ddd825c05957c84b02439b6a4b18
Nabeelkhan01 <nabeeltiger012@gmail.com>
2020-09-14 22:54:48 -0700

    Web Project

---
+++ Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NabeelKhan.Models
+{
+    public class Vehicle
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string model { get; set; }
+        public string price { get; set; }
+
+
+    }
+
+
+    public class VehicleContext : DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# NabeelWebProject-Asp" 
